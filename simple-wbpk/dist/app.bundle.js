59e52e46e75b4583e540769797a9194debe473e0
Unwana Essien <unwana.essien@outlook.com>
2018-01-27 17:01:16 +0100

    proof of webpack working

--- simple-wbpk/dist/app.bundle.js
+++ simple-wbpk/dist/app.bundle.js
@@ -68,7 +68,13 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
+var _express=__webpack_require__(1);var _express2=_interopRequireDefault(_express);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}var app=(0,_express2.default)();app.get('/',function(req,res){res.send('Da FOOQ is goiNG in ON');});app.listen(4000,function(){console.log('We are now connected to the port 4000!');});
 
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = require("express");
 
 /***/ })
 /******/ ]);
